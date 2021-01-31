f92510bb0519d15cea461cdaec654d15898d7a9a
Silas Almeida <silasadssn@gmail.com>
2021-01-31 14:50:58 -0300

    update component

--- frontend/src/components/Card/index.tsx
+++ frontend/src/components/Card/index.tsx
@@ -1,18 +1,25 @@
 import React from 'react'
 
+import './styles.css'
+
 interface IProps {
   title: string
   date: Date
   description: string
 }
 
-const Card: React.FC<IProps> = ({ title, description, date }) => {
+const Card: React.FC<IProps> = ({ title, description, date }: IProps) => {
   return (
-    <div className="card" style={{ width: '18rem' }}>
+    <div className="card shadow-lg rounded" style={{ width: '25rem' }}>
       <div className="card-body">
-        <h4 className="card-title">{title}</h4>
-        <h6 className="card-subtitle mb-2 text-muted">{date}</h6>
-        <p className="card-text">{description}</p>
+        <h4 className="classified card-title">{title.toUpperCase()}</h4>
+        <h6 className="classified card-subtitle mb-2 text-muted">
+          {`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()} - ${date.getUTCDate()}/${
+            date.getUTCMonth() + 1
+          }/${date.getUTCFullYear()}`}
+        </h6>
+
+        <p className="classified card-text">{description}</p>
       </div>
     </div>
   )
