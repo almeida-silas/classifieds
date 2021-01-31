ba57948fabc2afe1b5b8cc3c7e0e2f7cf048e318
Silas Almeida <silasadssn@gmail.com>
2021-01-31 14:54:39 -0300

    pages

--- frontend/src/pages/CreateClassified/index.tsx
+++ frontend/src/pages/CreateClassified/index.tsx
@@ -1,28 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import Header from '../../components/Header'
+import api from '../../services/api'
+
+import './styles.css'
 
 const CreateClassified: React.FC = () => {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  const handleCreate = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault()
+
+    try {
+      if (title.trim() && description.trim()) {
+        await api.post('/create', { title, description })
+
+        alert('O classificado foi escrito!')
+
+        location.href = '/'
+      }
+    } catch (error) {
+      return error
+    }
+  }
+
   return (
-    <form>
-      <div className="mb-3">
-        <label htmlFor="title" className="form-label">
-          Título
-        </label>
+    <>
+      <Header />
 
-        <input type="text" className="form-control" />
-      </div>
+      <form className="container create-classifieds shadow-lg">
+        <div className="form-group mb-3">
+          <label htmlFor="title">Título</label>
+          <input
+            type="text"
+            className="form-control"
+            value={title}
+            onChange={event => setTitle(event.target.value)}
+          />
+        </div>
 
-      <div className="mb-3">
-        <label htmlFor="description" className="form-label">
-          Descrição
-        </label>
+        <div className="form-group mb-3">
+          <label htmlFor="title">Descrição</label>
 
-        <textarea className="form-control" id="description" />
-      </div>
+          <textarea
+            className="form-control"
+            rows={8}
+            value={description}
+            onChange={event => setDescription(event.target.value)}
+          />
+        </div>
 
-      <button type="submit" className="btn btn-primary">
-        Confirmar
-      </button>
-    </form>
+        <button
+          type="submit"
+          className="btn btn-warning"
+          onClick={handleCreate}
+        >
+          Confirmar
+        </button>
+      </form>
+    </>
   )
 }
 
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,10 +4,16 @@ import { connect } from 'react-redux'
 import { bindActionCreators, Dispatch } from 'redux'
 
 import { ApplicationState } from '../../store'
+import { Classifieds } from '../../store/ducks/classifieds/types'
 import * as ClassifiedsActions from '../../store/ducks/classifieds/actions'
 
-import { FiPlusCircle } from 'react-icons/fi'
-import { Classifieds } from '../../store/ducks/classifieds/types'
+import Card from '../../components/Card'
+import Header from '../../components/Header'
+
+import Loader from 'react-loader-spinner'
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css'
+
+import './styles.css'
 
 interface StateProps {
   classifieds: Classifieds[]
@@ -15,35 +21,65 @@ interface StateProps {
 
 interface DispatchProps {
   loadRequest(): void
+  loading: boolean
+  error: boolean
 }
 
 type Props = StateProps & DispatchProps
 
 const Home: React.FC<Props> = (props: Props) => {
-  const { loadRequest } = props
+  const { loadRequest, loading, classifieds } = props
 
   useEffect(() => {
     loadRequest()
   }, [loadRequest])
 
+  classifieds.sort((a, b) => {
+    const dateA = new Date(a.date)
+    const dateB = new Date(b.date)
+
+    return dateB.valueOf() - dateA.valueOf()
+  })
+
   return (
-    <div>
-      <div>
-        <span>Classificados</span>
+    <div className="home-body">
+      <Header />
 
-        <button type="button" className="btn btn-secondary">
-          <FiPlusCircle size={22} /> <span>Novo classificado</span>
-        </button>
-      </div>
+      {!loading ? (
+        <>
+          <>
+            {classifieds.length > 0 ? (
+              <>
+                <div className="cards">
+                  {classifieds.map(({ id, title, date, description }) => (
+                    <div key={id} className="cards-body">
+                      <Card
+                        key={id}
+                        date={new Date(date)}
+                        title={title}
+                        description={description}
+                      />
+                    </div>
+                  ))}
+                </div>
 
-      <div></div>
-
-      <div className="alert alert-dark position-absolute bottom-0 start-50 translate-middle-x">
-        <div className="badge bg-secondary">
-          <span>4</span>
-        </div>{' '}
-        classificado(s)
-      </div>
+                <div className="alert position-absolute bottom-0 start-50 translate-middle-x">
+                  <div className="badge bg-secondary">
+                    <span>{classifieds.length}</span>
+                  </div>{' '}
+                  classificado(s)
+                </div>
+              </>
+            ) : (
+              <h1 className="no-content">Nenhum classificado por aqui!</h1>
+            )}
+          </>
+        </>
+      ) : (
+        <div className="loader">
+          <Loader type="Oval" color="#c8e7ff" height={200} width={200} />
+        </div>
+      )}
     </div>
   )
 }
